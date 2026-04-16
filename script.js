98a238607afe0318bf4e774fa813d1561cfa4443
Edoardo Paggi <edoardo.paggi.1997@gmail.com>
2026-04-16 11:39:58 +0200

    Create getHumanChoice() function and set scores

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function getComputerChoice() {
-    return Math.floor(Math.random() * 3); // Return 0, 1 or 2
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() { // number
+    // Return 0, 1 or 2
+    return Math.floor(Math.random() * 3);
 }
+
+function getHumanChoice() { // string
+    return prompt("Choose rock, paper or scissors: ");
+}
+
